6ff5017b5e6378d909882f084c9b8286737c4585
antonioayalam2001 <antonioayalam2001@gmail.com>
2023-10-13 23:26:08 -0600

    Agregando query para la base de datos

--- MiDuDev/Clase6/server/index.js
+++ MiDuDev/Clase6/server/index.js
@@ -3,9 +3,12 @@ import chat from '../routes/chat.js'
 import morgan from 'morgan'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client/.'
 const PORT = process.env.PORT || 3000
 const app = express()
 const server = createServer(app)
+dotenv.config()
 // En caso de que se pierda la conexión, se intentará reconectar durante 2 segundos.
 const io = new Server(server, {
   connectionStateRecovery: {
@@ -15,6 +18,30 @@ const io = new Server(server, {
 const routes = {
   chat: '/chat'
 }
+
+const db = createClient({
+  url: process.env.DATABASE_URL,
+  authToken: process.env.DATABASE_AUTH_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+  )
+`)
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+  )
+`)
+
 app.use(morgan('dev'))
 app.use(express.static('public'))
 app.use(routes.chat, chat)
